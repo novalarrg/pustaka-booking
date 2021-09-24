4c2c13e4203585e9aa429c96f8db8b5873ce9495
novalarrg <novaljr197@gmail.com>
2021-09-24 16:29:53 +0700

    menggeser komentar

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -21,7 +21,7 @@ class Contoh1 extends CI_Controller {
     public function index()
     {
         echo "<h1>Perkenalan</h1>";
-        echo "Nama saya Noval AbdurojaK <br>
+        echo "Nama saya Noval Abdurojak <br>
         Saya tinggal di daerah Tegal <br>
         Olah raga yang saya sukia adalah Volly";
     }
