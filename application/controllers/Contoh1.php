2ac616015d6e84b4309f5f45a4192964228ab72f
novalarrg <novaljr197@gmail.com>
2021-09-24 16:15:05 +0700

    upload (push) file-file project latihan codeigniter

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller {
+
+    /**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama saya Noval AbdurojaK <br>
+        Saya tinggal di daerah Tegal <br>
+        Olah raga yang saya sukia adalah Volly";
+    }
+}
