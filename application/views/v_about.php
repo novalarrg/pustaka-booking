7086ed2508251b3e8088d9397c41d389f769ae43
novalarg <novaljr197@gmail.com>
2021-10-08 12:26:50 +0700

    Membuat template web sederhana

---
+++ application/views/v_about.php
@@ -0,0 +1,28 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Noval</li>
+        <li>Nama Belakang : Abdurojak</li>
+    </ul>
+    <br>
+
+    
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jalan Krapyak No. 277 Sewon</li>
+    </ul>
+
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Tegal</li>
+    </ul>
+
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Volly</li>
+        <li>Sepak Bola</li>
+    </ul>
+</section>
