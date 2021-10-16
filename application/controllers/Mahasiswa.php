d3f43502d0772e934c11d3e8e9c8fd9c969c1d00
novalarg <novaljr197@gmail.com>
2021-10-17 00:21:04 +0700

    menambahkan validasi controller & view-form-matakuliah

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -1,14 +1,28 @@
 <?php 
 class Mahasiswa extends CI_Controller{
-    public function index(){
-        echo "Selemat Datang Mahasiswa";
+    public function index()
+    {
+        $this->load->view('view-form-matakuliah');
     }
 
-    public function biodata($nim="", $nama=""){
-        $data['nim']=$nim;
-        $data['nama']=$nama;
-        $this->load->view('Mahasiswa_view', $data);
-        //echo "Nim  : $nim <br>";
-        //echo "Nama : $nama ";
+    public function cetak()
+    { $this->form_validation->set_rules('kode','kode matakuliah','required|min_lenght[3]', ['required' => 'kode matakuliah harus diisi', 'min_lenght' => 'kode terlalu pendek']); 
+    
+    if ($this->form_validation->run() != true) {$this->load->view('view-from-matakuliah');
     }
+    else {
+        $data = [
+            'kode' => $this->input->post('kode'),
+            'nama' => $this->input->post('nama'),
+            'sks' => $this->input->post('sks')
+    ];
+
+    $this->load->view('view-data-matakuliah', $data);
+
+    }
+    
+    }
+    
+    
+    
 }
--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -7,12 +7,34 @@ class Matakuliah extends CI_Controller {
 
     public function cetak()
     {
-        $data = [
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('nama'),
-            'sks' => $this->input->post('sks')
-        ];
+        $this->form_validation->set_rules('kode','kode matakuliah','trim|required|min_lenght[3]',
+        array(
+            'required' => '%s Wajib diisi.',
+            'min_lenght' => '%s Minimal 3 karakter'
+        ));
 
-        $this->load->view('view-data-matakuliah', $data);
+        $this->form_validation->set_rules('nama','nama matakuliah','required',
+        array(
+            'required' => '%s Wajib diisi.'
+        ));
+
+        $this->form_validation->set_rules('sks','sks','required',
+        array(
+            'required' => '%s Wajib diisi.'
+        ));
+
+        if ($this->form_validation->run() == false)
+        {
+            $this->load->view('view-form-matakuliah');
+        }
+        else {
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks')
+            ];
+
+            $this->load->view('view-data-matakuliah', $data);
+        }
     }
 }
