f2d92aabfefc07fa68354e5e7679e829e96f1c7d
novalarrg <novaljr197@gmail.com>
2021-11-17 08:36:14 +0700

    Tugas Pertemuan 9 Login

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -1,5 +1,6 @@
-<?php 
-class Matakuliah extends CI_Controller {
+<?php
+class Matakuliah extends CI_Controller
+{
     public function index()
     {
         $this->load->view('view-form-matakuliah');
@@ -7,27 +8,37 @@ class Matakuliah extends CI_Controller {
 
     public function cetak()
     {
-        $this->form_validation->set_rules('kode','kode matakuliah','trim|required|min_lenght[3]',
-        array(
-            'required' => '%s Wajib diisi.',
-            'min_lenght' => '%s Minimal 3 karakter'
-        ));
+        $this->form_validation->set_rules(
+            'kode',
+            'Kode',
+            'trim|required|min_length[3]',
+            array(
+                'required' => '%s Wajib diisi.',
+                'min_lenght' => '%s terlalu pendek'
+            )
+        );
 
-        $this->form_validation->set_rules('nama','nama matakuliah','required',
-        array(
-            'required' => '%s Wajib diisi.'
-        ));
+        $this->form_validation->set_rules(
+            'nama',
+            'Nama Matakuliah',
+            'required',
+            array(
+                'required' => '%s Wajib diisi.'
+            )
+        );
 
-        $this->form_validation->set_rules('sks','sks','required',
-        array(
-            'required' => '%s Wajib diisi.'
-        ));
+        $this->form_validation->set_rules(
+            'sks',
+            'SKS',
+            'required',
+            array(
+                'required' => '%s Wajib diisi.'
+            )
+        );
 
-        if ($this->form_validation->run() == false)
-        {
+        if ($this->form_validation->run() == false) {
             $this->load->view('view-form-matakuliah');
-        }
-        else {
+        } else {
             $data = [
                 'kode' => $this->input->post('kode'),
                 'nama' => $this->input->post('nama'),
